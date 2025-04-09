6d04fed835e2485bab5dccbe385152fdc1afde8a
vikshitchauhan <vikshitrajpoot@gmail.com>
2025-04-09 18:07:49 +0530

    Initial commit

---
+++ src/main/java/com/example/virtualcare/VirtualcareApplication.java
@@ -0,0 +1,13 @@
+package com.example.virtualcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/virtualcare/VirtualcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.virtualcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
