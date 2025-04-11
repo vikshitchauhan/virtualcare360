21852e3fa5bad6db8cb514c5bf5d4f9862b48af0
vikshitchauhan <vikshitrajpoot@gmail.com>
2025-04-11 20:25:36 +0530

    Tailwind

--- src/main/java/com/example/virtualcare/controller/PageController.java
+++ src/main/java/com/example/virtualcare/controller/PageController.java
@@ -7,18 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
 
-  @GetMapping("/")
-    public static String index(){
-        return "redirect:/home";
-        }
 
 
-         @RequestMapping("/home")
-    public String home(Model model){
-        System.out.println("Home page handler");
-        model.addAttribute("name","vikshit");
-        model.addAttribute("githubrepository","https://leetcode.com/problems/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/solutions/1251853/simple-java-solution-backtracking/");
-        return "home";
-    }
+    
+  @GetMapping("/home")
+  public String homePage() {
+      return "home"; // This renders templates/home.html
+  }
 
 }
